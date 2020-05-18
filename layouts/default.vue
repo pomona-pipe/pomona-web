22e2b8325b5e89bd821284b5069958f8d3fdf5ff
BENZABOSS <joyceben33@gmail.com>
2020-05-18 16:58:12 -0400

    Navigation dropdown with static links.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,36 @@
 <template>
   <v-app>
     <v-app-bar app fixed color="#303030" dark shrink-on-scroll dense>
-      <img src="~/assets/logo_xlarge.png" class="pomona_logo"/>
-      <v-row justify="center" no-gutters>
-        <v-btn
-          v-for="link in navLinks"
-          :key="link"
-          :nuxt="true"
-          :to="link.to"
-          color="white"
-          text
-          rounded
-          class="my-2"
-        >{{ link.name }}</v-btn>
-      </v-row>
+        <img src="~/assets/logo_xlarge.png"  class="pomona_logo"/> 
+      <div>
+        <v-menu v-for="link in navLinks" :key="link" open-on-hover top offset-y>
+          <!-- non-repeat -->
+          <template v-slot:activator="{ on }">
+            <v-btn
+              :nuxt="true"
+              :to="{path: `/${link['non-repeat'].link.value.document.uid}`}"
+              color="primary"
+              dark
+              v-on="on"
+            >{{ link['non-repeat'].label.value[0].text }}</v-btn>
+          </template>
+
+          <!-- repeat -->
+          <v-list>
+            <v-list-item
+              v-for="dropdown in link.repeat"
+              :key="dropdown"
+              :nuxt="true"
+              :to="{path: `/products/${dropdown.sub_nav_link.value.document.uid}`}"
+              text
+              rounded
+              class="my-2"
+            >
+              <v-list-item-title>{{ dropdown.sub_nav_link_label.value[0].text }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
       <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
@@ -25,7 +42,7 @@
       </v-container>
     </v-content>
 
-    <v-footer color="#303030" padless>
+    <!-- <v-footer color="#303030" padless>
       <v-row justify="center" no-gutters>
         <v-btn
           v-for="link in navLinks"
@@ -42,12 +59,16 @@
           <strong>Vuetify</strong>
         </v-col>
       </v-row>
-    </v-footer>
+    </v-footer>-->
   </v-app>
 </template>
 <style lang="css" scoped>
 .my-2 {
   display: flex;
+}
+.pomona_logo_link {
+  max-height: 50%;
+  margin: auto;
 }
 .pomona_logo {
   max-height: 50%;
@@ -63,7 +84,5 @@ import { mapState } from 'vuex'
   components: {},
   computed: mapState('layout', ['navLinks'])
 })
-export default class DefaultLayout extends Vue {
- 
-}
+export default class DefaultLayout extends Vue {}
 </script>
--- store/layout.js
+++ store/layout.js
@@ -1,13 +1,56 @@
 export const state = () => ({
   mobileDrawer: false,
-  navLinks: [
-    { name: 'Bridges & Structures', to: '/' },
-    { name: 'Pipe', to: '/inspire' },
-    { name: 'Stormwater Management', to: '/todos' },
-    { name: 'Geosynthetics', to: '/inspire' },
-    { name: 'Masonry', to: '/inspire' },
-    { name: 'Other Products', to: '/inspire' }
-  ],
+  navLinks: [{
+    repeat: [{
+        sub_nav_link_label: {
+          type: "StructuredText",
+          value: [{
+            type: "paragraph",
+            text: "Bridges & Structures",
+            spans: []
+          }]
+        },
+        sub_nav_link: {
+          type: "Link.document",
+          value: {
+            document: {
+              id: "XrmovRAAAPuZs9HK",
+              type: "product_categories",
+              tags: [],
+              lang: "en-us",
+              slug: "bridges--structures",
+              uid: "bridges-and-structures"
+            },
+            isBroken: false
+          }
+        }
+      }
+
+    ],
+    "non-repeat": {
+      label: {
+        value: [{
+          type: "paragraph",
+          text: "Products",
+          spans: [ ]
+        }]
+      },
+      link: {
+        type: "Link.document",
+        value: {
+          document: {
+            id: "XsLLnxAAAMq025VU",
+            type: "category_page",
+            tags: [],
+            lang: "en-us",
+            slug: "product-categories",
+            uid: "products",
+          },
+          isBroken: false
+        }
+      }
+    }
+  }],
 })
 
 export const mutations = {
