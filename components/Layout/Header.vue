4cfc04a18ff4d8c3259c8ced2334e1342e08a499
BENZABOSS <joyceben33@gmail.com>
2020-07-29 14:53:04 -0400

    Added Pomona Logo to Static folder.

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -20,7 +20,7 @@
       <!-- Company Logo -->
       <div>
         <nuxt-link to="/">
-          <img src="~/assets/logo_xlarge.png" class="pomona_logo" />
+          <img src="/logo_xlarge.png" class="pomona_logo" />
         </nuxt-link>
       </div>
       <div class="d-flex">
