a0c535376c82ed48b159d055933fc9c3817de45a
Dillon Joyce <dmjoyce92@gmail.com>
2022-02-10 23:05:23 -0500

    Cleanup

--- app/jest.setup.ts
+++ app/jest.setup.ts
@@ -1,10 +1,8 @@
 import Vue from 'vue'
-// import Vuetify from 'vuetify'
 import { config } from '@vue/test-utils'
 
 Vue.config.silent = true
 
-// Vue.config.ignoredElements = ['nuxt-link']
 // Mock Nuxt components
 config.stubs.nuxt = { template: '<div />' }
 config.stubs['nuxt-link'] = { template: '<a><slot /></a>' }
