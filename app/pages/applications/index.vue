6b2c592fa46b0c6e96437e039f3e6285df7cb2b7
Dillon Joyce <dmjoyce92@gmail.com>
2022-02-18 15:00:12 -0500

    Applications listing page - add image optimization

--- app/pages/applications/index.vue
+++ app/pages/applications/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <!-- Hero -->
-    <section class="hero" :style="heroStyles">
+    <section class="hero">
+      <v-img
+        :src="heroImg.src"
+        :srcset="heroImg.srcset"
+        :sizes="heroImg.sizes"
+        :gradient="theme.dark ? theme.themes.dark.heroGradient : theme.themes.light.heroGradient"
+      />
       <v-container>
         <v-row align="center" class="fill-height">
           <v-col align="center">
@@ -18,7 +24,7 @@
         <!-- template for product category cards -->
         <v-row>
           <v-col
-            v-for="app in applications"
+            v-for="(app, index) in applications"
             :key="app.id"
             cols="12"
             sm="6"
@@ -32,7 +38,9 @@
               height="100%"
             >
               <v-img
-                :src="app.data.hero_image.fileUrl || placeholders.file"
+                :src="cardImgs[index].src"
+                :srcset="cardImgs[index].srcset"
+                :sizes="cardImgs[index].sizes"
                 height="200px"
               ></v-img>
 
@@ -48,28 +56,63 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import { Store, mapState } from 'vuex'
+import { createImgSrcset, createImgSizes } from '~/services/imgOptimization'
 import pageVisits from '~/services/pageVisits'
 import { IPrismic } from '~/shims'
 
 @Component({
   components: {},
   computed: {
-    ...mapState('layout', ['placeholders']),
+    ...mapState('layout', ['placeholders', 'theme']),
     ...mapState('pages', ['applicationsPage']),
     ...mapState('applications', ['applications']),
-    heroStyles() {
-      return {
-        'background-image': `linear-gradient(to right top, rgba(36, 36, 36, 0.9), rgba(25, 32, 72, 0.7)), url("${
-          (this as any).$store.state.pages.applicationsPage[0].data.hero_image
-            .fileUrl
-        }")`,
-        'background-position': 'center',
-        'background-size': 'cover'
-      }
-    }
   }
 })
 export default class Index extends Vue {
+  get heroImg() {
+    const url = (this as any).applicationsPage[0].data.hero_image.fileUrl;
+    if(!url) {
+      return {
+        src: '',
+        srcSet: '',
+        sizes: '',
+      }
+    }
+    return {
+      src: url,
+      srcset: createImgSrcset(url),
+      sizes: createImgSizes(),
+    }
+  }
+
+  get cardImgs() {
+    const cardUrls = (this as any).applications.map((app: any) => app.data.hero_image.fileUrl);
+    const placeholder = {
+      src: (this as any).placeholders.file,
+      srcSet: '',
+      sizes: '',
+    };
+    if(cardUrls.length === 0) {
+      return [placeholder]
+    }
+    const cols = {
+      xs: 12,
+      sm: 6,
+      md: 4,
+      lg: 3,
+    };
+    return cardUrls.map((url?: string) => {
+      if(!url) {
+        return placeholder;
+      }
+      return {
+        src: url,
+        srcset: createImgSrcset(url, cols),
+        sizes: createImgSizes(cols),
+      };
+    })
+  }
+
   head() {
     return {
       title: (this as any).applicationsPage[0].data.title_tag,
