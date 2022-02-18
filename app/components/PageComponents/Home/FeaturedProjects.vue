0a0e8d823ad789459ed510ef97756baded9b5c7a
Dillon Joyce <dmjoyce92@gmail.com>
2022-02-18 16:18:45 -0500

    Homepage Featured Projects - add image optimization

--- app/components/PageComponents/Home/FeaturedProjects.vue
+++ app/components/PageComponents/Home/FeaturedProjects.vue
@@ -6,7 +6,7 @@
       <h2 class="text-center">Featured Projects:</h2>
       <v-row class="mt-12">
         <v-col
-          v-for="(project) in projects"
+          v-for="(project, index) in projects"
           :key="project.id"
           cols="12"
           sm="6"
@@ -20,10 +20,11 @@
             class="d-flex flex-column justify-space-between"
           >
             <v-img
-              :src="
-                project.data.hero_image.fileUrl || placeholders.file
-              "
-            height="200px"></v-img>
+              :src="cardImgs[index].src"
+              :srcset="cardImgs[index].srcset"
+              :sizes="cardImgs[index].sizes"
+              height="200px"
+            ></v-img>
 
             <v-card-title>{{ project.data.name[0].text }}</v-card-title>
             <v-card-text class="text--primary">{{
@@ -58,6 +59,7 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 import { mapState } from 'vuex'
 import moment from 'moment'
+import { createImgSrcset, createImgSizes } from '~/services/imgOptimization'
 
 @Component({
   components: {},
@@ -71,6 +73,35 @@ export default class FeaturedProjects extends Vue {
   get projects() {
     return this.$store.state.projects.projects.slice(0, 3);
   }
+
+  get cardImgs() {
+    const cardUrls = this.projects.map((project: any) => project.data.hero_image.fileUrl);
+    const placeholder = {
+      src: (this as any).placeholders.file,
+      srcSet: '',
+      sizes: '',
+    };
+    if(cardUrls.length === 0) {
+      return [placeholder]
+    }
+    const cols = {
+      xs: 12,
+      sm: 6,
+      lg: 4,
+    };
+    return cardUrls.map((url?: string) => {
+      if(!url) {
+        return placeholder;
+      }
+      return {
+        src: url,
+        srcset: createImgSrcset(url, cols),
+        sizes: createImgSizes(cols),
+      };
+    })
+  }
+
+
   formatDateString(dateString: string) {
     return moment(dateString).format('MMMM YYYY')
   }
